5d76f421c7f5193d656b2dd9c65fcea24bea2fc1
ChengNing <351200797@qq.com>
2020-09-27 15:59:25 +0800

    添加了推荐优先队列

--- social/logics.py
+++ social/logics.py
@@ -7,8 +7,15 @@ from user.models import Profile
 from social.models import Swiped, Friend
 
 
-def rcmd(uid):
-    '''推荐滑动用户'''
+def rcmd_from_queue(uid):
+    '''从优先推荐队列进行推荐'''
+    uid_list = rds.lrange(keys.FIRST_RCMD_Q % uid, 0, 19)  # 从优先推荐队列取出 uid 列表
+    uid_list = [int(uid) for uid in uid_list]  # 将 uid 强转成 int 类型
+    return User.objects.filter(id__in=uid_list)
+
+
+def rcmd_from_db(uid, num=20):
+    '''从数据库中推荐滑动用户'''
     profile = Profile.objects.get(id=uid)
 
     #   计算出生日期范围
@@ -23,11 +30,23 @@ def rcmd(uid):
         location=profile.dating_location,
         gender=profile.dating_gender,
         birthday__range=[earliest_birth, latest_birth],
-    ).exclude(id__in=sid_list)[:20]  # 懒加载
+    ).exclude(id__in=sid_list)[:num]  # 懒加载
 
     # TODO：排除已经滑过的人
 
     return users
+
+
+def rcmd(uid):
+    '''推荐滑动用户'''
+    first_users = rcmd_from_queue(uid)
+    remain = 20 - len(first_users) # 计算需要从数据库获取的个数
+    if remain:
+        second_users = rcmd_from_db(uid,remain)
+        return  set(first_users) | set(second_users)
+    else:
+        return first_users
+
 
 
 def like_someone(uid, sid):
@@ -35,6 +54,9 @@ def like_someone(uid, sid):
 
     # 添加滑动记录
     Swiped.objects.create(uid=uid, sid=sid, stype='like')
+
+    # 强制删除优先推荐队列中的 sid
+    rds.lrem(keys.FIRST_RCMD_Q % uid, count=0, value=sid)
 
     # 检查对方是否喜欢(右滑或上滑)过自己
     if Swiped.is_liked(sid, uid):
@@ -51,6 +73,9 @@ def superlike_someone(uid, sid):
     # 添加滑动记录
     Swiped.objects.create(uid=uid, sid=sid, stype='superlike')
 
+    # 强制删除优先推荐队列中的 sid
+    rds.lrem(keys.FIRST_RCMD_Q % uid, count=0, value=sid)
+
     # 检查对方是否喜欢(右滑或上滑)过自己
     liked = Swiped.is_liked(sid, uid)
     if liked is True:
@@ -62,3 +87,4 @@ def superlike_someone(uid, sid):
     else:
         # 对方尚未滑到过自己，把自己推荐给对方
         rds.rpush(keys.FIRST_RCMD_Q % sid, uid)
+        return False
